36ed96132f2b01399dd9597bfb102ae32b6b64a7
Sungmin Kang <stuatlittle@kaist.ac.kr>
2019-04-28 21:04:46 +0900

    updated crawler to be faster

--- crawler.py
+++ crawler.py
@@ -21,22 +21,22 @@ pw_input.send_keys(Keys.RETURN)
 time.sleep(3)
 driver.get("https://mobile.facebook.com/KaDaejeon/")
 
-f = codecs.open("ok.txt", "w", "utf-8")
+f = codecs.open("ok2.txt", "w", "utf-8")
 
 found_hrefs = set()
-for s_idx in range(200):
+for s_idx in range(1000):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(3)
+    time.sleep(2)
 
-    stuff = driver.find_elements_by_class_name("_15kq")
-    for e_idx, elem in enumerate(stuff):
-        e_href = elem.get_attribute("href")
-        
-        if e_href in found_hrefs:
-            continue
-        else:
-            f.write(("[%d]" % e_idx) + e_href)
-            f.write("\r\n")
-            found_hrefs.add(e_href)
+stuff = driver.find_elements_by_class_name("_15kq")
+for e_idx, elem in enumerate(stuff):
+    e_href = elem.get_attribute("href")
+    
+    if e_href in found_hrefs:
+        continue
+    else:
+        f.write(("[%d]" % e_idx) + e_href)
+        f.write("\r\n")
+        found_hrefs.add(e_href)
 
 f.close()
